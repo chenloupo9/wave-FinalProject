c478727e85fefbfb3bb27f23f0a4f74757abc849
chenloupo9 <loupo.chen@gmail.com>
2021-11-03 22:48:55 +0000

    files

--- files/terraform-installition.sh
+++ files/terraform-installition.sh
@@ -6,7 +6,3 @@ sudo apt update
 sudo apt install terraform 
 terraform -install-autocomplete 
 source /home/dev/.bashrc 
-echo export AWS_ACCESS_KEY_ID="<your_access_key>" >> ~/.bashrc
-echo export AWS_SECRET_ACCESS_"<your_secret_key" >> ~/.bashrc
-echo export AWS_DEFAULT_REGION=eu-central-1 >> ~/.bashrc
-source ~/.bashrc
